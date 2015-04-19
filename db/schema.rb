8b36c7f6fa8fe2fe9335186496b5bef7fe632725
Tobias Witt <tobias.witt@hhu.de>
2015-04-19 11:37:13 +0200

    Add comment column to ratings table

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150412124812) do
+ActiveRecord::Schema.define(version: 20150419083537) do
 
   create_table "ratings", force: :cascade do |t|
     t.string "member"
     t.string "milestone"
     t.string "ko"
+    t.string "comment"
   end
 
   add_index "ratings", ["member", "milestone"], name: "index_ratings_on_member_and_milestone", unique: true
