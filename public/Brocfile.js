8593036695aa42863dbd4c1892873018d4567944
Tobias Witt <tobias.witt@hhu.de>
2015-05-01 17:02:48 +0200

    Install Chart.js

--- public/Brocfile.js
+++ public/Brocfile.js
@@ -15,6 +15,8 @@ var app = new EmberApp({
 app.import("bower_components/bootstrap/dist/css/bootstrap.css");
 app.import("bower_components/bootstrap/dist/css/bootstrap-theme.css");
 
+app.import("bower_components/Chart.js/Chart.js");
+
 // Use `app.import` to add additional libraries to the generated
 // output files.
 //
